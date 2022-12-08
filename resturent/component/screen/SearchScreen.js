742b4da23a16fe541e8ec25da42ff9859099dadf
bishal42 <sthabishal422@gmail.com>
2022-12-08 08:44:33 +0100

    initilizing the search when the app load first time to display the data

--- resturent/component/screen/SearchScreen.js
+++ resturent/component/screen/SearchScreen.js
@@ -1,36 +1,45 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {View,Text,StyleSheet,Button} from 'react-native'
 import SearchBar from '../SearchBar';
 import yelp from '../../src/api/yelp';
 
 
 const SearchScreen = () => {
-    const [term,setTerm]= useState('');
-    const [result,setResult] = useState([]);
-    const [errorMessage, setErrorMessage]= useState ('');
+    const [term,setTerm]= useState(''); //this state handles the search iteams
+    const [result,setResult] = useState([]); // thsi state handles the data from api
+    const [errorMessage, setErrorMessage]= useState (''); // this state handles the error message
 
-    const searchApi = async ()=>{
+    //using asynchronous function to handle the api request
+    const searchApi = async (searchTerm)=>{
+        
         try{
         const response = await yelp.get('/search',{
             params: {
                 limit:50,
-                term : term,
+                term : searchTerm,
                 location: 'san joes'
             }
         });
-        setResult(response.data.businesses);
+        setResult(response.data.businesses);// update the business array in result state
     }
     catch (err) {
-        setErrorMessage('something went wrong')
+        setErrorMessage('something went wrong') // it throw a error message when something went wrong in yelp component
     }
     };
+
+    //use useEffect for search at the first time whem app load 
+    useEffect(()=>{
+        searchApi('noodles');
+    },[])
     
     return(
     <View>
         <SearchBar 
         term={term} 
         onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit ={searchApi}
+        onTermSubmit ={()=>{
+            searchApi(term)
+        }}
         />
         <Text style={{color:'red'}}>{errorMessage}</Text>
         <Text>we Found {result.length} results</Text>
--- resturent/src/api/yelp.js
+++ resturent/src/api/yelp.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
+// using axios to fetch the data from api
 export default axios.create ({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers:{
-        Authorization: 'Bearerpxm33PbWFQfa9v9J_90SinnlhI4bDmSJFfvivkwUKVmQ_M7oEWRYPl7jyr63uejIOdwjEzqLCnev4eGIjeLHrFlq9YWrAcqjZrrqbeJQk7R9te03S-zy3DOo8yuQY3Yx'
+        Authorization: 'Bearer pxm33PbWFQfa9v9J_90SinnlhI4bDmSJFfvivkwUKVmQ_M7oEWRYPl7jyr63uejIOdwjEzqLCnev4eGIjeLHrFlq9YWrAcqjZrrqbeJQk7R9te03S-zy3DOo8yuQY3Yx'
     }
 });
